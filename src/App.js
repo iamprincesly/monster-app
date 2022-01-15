a0858df2d76a3f101bc16c714d2e0c6063f56e14
iamprincesly <iamprincesly@gmail.com>
2022-01-15 12:22:06 +0100

    Implement search filter for monster

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
         this.state = {
             monsters: [],
+            searchField: '',
         };
     }
 
@@ -18,9 +19,20 @@ class App extends Component {
     }
 
     render() {
+        const { monsters, searchField } = this.state;
+        const filteredMonsters = monsters.filter((monster) =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+        );
         return (
             <div className='App'>
-                <CardList monsters={this.state.monsters} />
+                <input
+                    type='search'
+                    placeholder='Search monsters'
+                    onChange={(e) => {
+                        this.setState({ searchField: e.target.value });
+                    }}
+                />
+                <CardList monsters={filteredMonsters} />
             </div>
         );
     }
